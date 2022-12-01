f187ae245e73289f221ba84c7f7679effad7b448
Srushti <srushti1010shah@gmail.com>
2022-12-01 14:43:30 +0530

    show and hide on click logic added to styling

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   const [isClicked, setIsClicked] = useState(false);
   return (
     <>
-      <div className="container py-12 w-screen px-12 bg-gray-200 max-w-screen-2xl max-h-screen h-screen">
+      <div className="container py-32 w-screen px-12 bg-gray-200 max-w-screen-2xl max-h-screen h-screen">
         <div className="container mx-auto bg-white w-1/2 h-max p-4 rounded-lg flex flex-col gap-3">
           <div className="w-full h-12">
             <SearchBar isClicked={isClicked} setIsClicked={setIsClicked} />
           </div>
-          <div className="w-full h-full">{isClicked ? <>Hello</> : ""}</div>
+          <div className={`w-full h-full ${isClicked ? "block" : "hidden"}`}>
+            Hello
+          </div>
         </div>
       </div>
     </>
