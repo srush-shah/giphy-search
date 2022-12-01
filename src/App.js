c1fbf33f579fc8f93ba62ded46438bf5335f9128
Srushti <srushti1010shah@gmail.com>
2022-12-01 13:52:08 +0530

    Searchbar UI created and added to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+//Component
+import SearchBar from "./components/SearchBar/SearchBar.component";
+
 function App() {
-  return <div className="text-2xl">Hi</div>;
+  return (
+    <>
+      <div className="container py-12 w-screen px-12 bg-gray-200 max-w-screen-2xl max-h-screen h-screen">
+        <div className="container mx-auto bg-white w-1/2 h-20 p-4 rounded-lg">
+          <SearchBar />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+
+const SearchBar = () => {
+  return (
+    <>
+      <div className="flex items-center gap-3 w-full h-full">
+        <div className="bg-gray-200 flex items-center justify-between rounded-lg w-5/6 h-full">
+          <BiSearch className="w-1/6 h-2/5" />
+          <input
+            className="bg-gray-200 w-full h-full rounded-lg"
+            type="search"
+          />
+        </div>
+        <div className="w-1/5 h-full">
+          <button className="bg-black w-full h-full text-white rounded-lg">
+            Search
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
